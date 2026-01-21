83afd8a1e987038eedd2a7dbdf8229293c913144
yv-was-taken <yvmail@proton.me>
2026-01-21 17:30:05 -0600

    Update Homebrew formula sha256 for v0.1.2

--- packaging/homebrew/desktop-waifu.rb
+++ packaging/homebrew/desktop-waifu.rb
@@ -5,7 +5,7 @@ class DesktopWaifu < Formula
   desc "Animated 3D VRM characters with AI-powered conversational chat"
   homepage "https://github.com/yv-was-taken/desktop-waifu"
   url "https://github.com/yv-was-taken/desktop-waifu/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "ce4164a314a5faf390e63b8dfc55573dc3ec8dcb67cdc44aa72cbda2e166f20f"
+  sha256 "4d313ae931b2acc17b2a646b23f7e8d8644b804b2a785213bec1d5b20c74e7f3"
   license "MIT"
   head "https://github.com/yv-was-taken/desktop-waifu.git", branch: "master"
 
