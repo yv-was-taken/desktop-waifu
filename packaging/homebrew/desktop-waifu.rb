2605b504efa20222d434d55ce2e5d84cc6f08492
yv-was-taken <yvmail@proton.me>
2026-01-21 17:11:08 -0600

    Bump version to 0.1.1
    - Add font size setting to settings modal

--- packaging/homebrew/desktop-waifu.rb
+++ packaging/homebrew/desktop-waifu.rb
@@ -4,8 +4,8 @@
 class DesktopWaifu < Formula
   desc "Animated 3D VRM characters with AI-powered conversational chat"
   homepage "https://github.com/yv-was-taken/desktop-waifu"
-  url "https://github.com/yv-was-taken/desktop-waifu/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "caa1151e7f6e5d91f678d84797f9a8ba8c371701d961096946e7649e5f1357eb"
+  url "https://github.com/yv-was-taken/desktop-waifu/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 ""  # TODO: Update with checksum after creating v0.1.1 release
   license "MIT"
   head "https://github.com/yv-was-taken/desktop-waifu.git", branch: "master"
 
